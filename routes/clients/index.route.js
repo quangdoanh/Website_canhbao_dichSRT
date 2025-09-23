14699e94ea7718cc37ce79a3fb436dd5847bcc35
tuepham634 <152422581+tuepham634@users.noreply.github.com>
2025-09-24 00:50:32 +0700

    ẩn hiện bài viết(1bai)+ mất rừng đã xác nhận mới có view bản đồ + phần contact trang list câu hỏi và trả lời

--- routes/clients/index.route.js
+++ routes/clients/index.route.js
@@ -3,12 +3,13 @@ const route = require('express').Router();
 const homeRouter = require(`./home.route`);
 const contactRouter = require('./contact.route');
 const aboutRouter = require('./about.route');
+const faqRouter = require('./faq.routes');
 
 const authMiddleware = require("../../middlewares/clients/auth.middlewares");
 
 route.use('/', homeRouter)
 route.use('/contact', contactRouter)
 route.use('/about-us', aboutRouter)
-
+route.use('/faq', faqRouter); 
 
 module.exports = route;
