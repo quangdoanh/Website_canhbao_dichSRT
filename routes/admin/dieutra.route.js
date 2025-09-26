f02001b75cab94024aa9b4215a9dc38c73d1af0e
tuepham634 <152422581+tuepham634@users.noreply.github.com>
2025-09-26 22:20:38 +0700

    thêm api listdieutra

--- routes/admin/dieutra.route.js
+++ routes/admin/dieutra.route.js
@@ -6,6 +6,8 @@ const cloudinaryRarHelper = require('../../helpers/cloudinary_rar.helper');
 const upload = multer({ storage: cloudinaryRarHelper.storageRar });
 // =========== ĐIỀU TRA ======== //
 router.get('/list', DieuTraController.listDieuTra);
+router.get('/api/list', DieuTraController.appListDieuTra);
+
 router.get('/create', DieuTraController.createDieuTra);
 router.post('/create', DieuTraController.createDieuTraPost);
 router.get('/edit/:id', DieuTraController.editDieuTra);
